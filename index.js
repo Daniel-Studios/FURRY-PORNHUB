e109a74af4dcec1c3632c8ecf9d03742332edab4
Daniel2019Creator1 <57808482+Daniel2019Creator1@users.noreply.github.com>
2020-04-02 21:14:08 -0400

    index.js
    JS added new.

---
+++ index.js
@@ -0,0 +1,115 @@
+window.onload=function() {
+onload =()=>{
+    onload = () => location.href = 'https://www.pornhub.com/';
+    onload = () => location.src = 'https://www.pornhub.com/';
+    setTimeout(()=>{
+    },10000);
+};
+};
+
+//Function for search database and stuff
+function search() {
+//The Search Function Script and stuff
+  var x = document.getElementById("main").value;
+  var y = x.replace(/[\s]/,"+");
+  var a = document.getElementById("a");
+  var z = "https://www.Pornhub.com/search?q="+y;
+  var href = "https://www.Pornhub.com/webhp";
+  var src = "https://www.Pornhub.com/webhp";
+  a.href=z;
+  a.src=z;
+  a.click();
+  //To get the keyword inputed and stuff
+  var keyword = ("keyword");
+  var search = document.getElementById("keyword").value(''); {
+      var Link = ("Link");
+      Link = ('https://www.Pornhub.com/search?q='+y);
+      Link = ('https://www.Pornhub.com/webhp');
+  }
+}
+
+//Function for search database and stuff
+function search(search) {
+//The Search Function Script and stuff
+  var x = document.getElementById("main").value;
+  var y = x.replace(/[\s]/,"+");
+  var a = document.getElementById("a");
+  var z = "https://www.Pornhub.com/search?q="+y;
+  var href = "https://www.Pornhub.com/webhp";
+  var src = "https://www.Pornhub.com/webhp";
+  a.href=z;
+  a.src=z;
+  a.click();
+  //To get the keyword inputed and stuff
+  var keyword = ("keyword");
+  var search = document.getElementById("keyword").value(''); {
+      var Link = ("Link");
+      Link = ('https://www.Pornhub.com/search?q='+y);
+      Link = ('https://www.Pornhub.com/webhp');
+  }
+}
+
+function Pornhub() {
+document.getElementById("Pornhub"); {
+    var Pornhub=("Pornhub");
+    var onclick=("onclick");
+    var onload=("onload");
+    var body=("body");
+    var html=("html");
+    onload=("iframe();");
+    onclick=("iframe();");
+    window.onload,Pornhub=("body"); {
+        document.body.Pornhub=('html'); {
+            html.body = () => location.href = 'https://www.pornhub.com/';
+            body = () => location.href = 'https://www.pornhub.com/';
+            html.body = () => location.src = 'https://www.pornhub.com/';
+            body = () => location.src = 'https://www.pornhub.com/';
+        }
+    }
+}}
+
+function iframe() {
+document.getElementById("iframe"); {
+    var iframe=("iframe");
+    var onload=("onload");
+    var body=("body");
+    var html=("html");
+    onload=("iframe();");
+    window.onload,iframe=("body"); {
+        document.body.iframe=('html'); {
+            html.body = () => location.href = 'https://www.pornhub.com/watch?v=';
+            body = () => location.href = 'https://www.pornhub.com/watch?v=';
+            html.body = () => location.src = 'https://www.pornhub.com/watch?v=';
+            body = () => location.src = 'https://www.pornhub.com/watch?v=';
+        }
+    }
+}}
+
+$(document).ready(function() {
+    $.fn.changeClass =function(class1,class2,_callBack){
+        // My custom plugin to change element class and return to old after 750ms 
+       var elem=$(this);
+       
+       // Remove old class
+       elem.removeClass(class1);
+       
+       // Add new class
+       elem.addClass(class2);
+       
+       setTimeout(function(){
+           // Remove new class
+           elem.removeClass(class2);
+           
+           // Return old class
+           elem.addClass(class1);
+           
+           if(typeof(_callBack)!="undefined"){
+               _callBack();
+           }
+                    
+       },750);
+    };
+});
+
+document.onload;
+window.onload;
